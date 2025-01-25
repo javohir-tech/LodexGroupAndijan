2f5394c245ee3fefb4c37ac62c4fc0a0ca353866
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-01-25 20:15:57 +0500

    update

--- src/Sections/Banner.jsx
+++ src/Sections/Banner.jsx
@@ -1,11 +1,8 @@
 // react bootstrap
 import { Button, Container, Form, InputGroup } from 'react-bootstrap'
 
-//components
+//components 
 import { PlayMarket } from '../Components'
-
-//react icons
-import { FaGooglePlay, FaApple } from "react-icons/fa";
 
 //image
 import BannerImg from '../assets/bannerImg.png'
@@ -22,20 +19,6 @@ export default function Banner() {
                     <p className='text-secondary'>
                        {t("banner.title1")} <span> {t("banner.title2")}</span>
                     </p>
-                    <InputGroup className="my-4 w-75">
-                        <Form.Control
-                            placeholder="Enter your email"
-                            aria-label="Recipient's username"
-                            aria-describedby="basic-addon2"
-                        />
-                        <Button variant="danger" id="button-addon2">
-                            Send
-                        </Button>
-                    </InputGroup>
-                    <div className='d-flex gap-4'>
-                        <PlayMarket icon={<FaGooglePlay style={{ fontSize: "24px" }} />} title={"Android App on"} subtitle={"Google Play"} />
-                        <PlayMarket icon={<FaApple style={{ fontSize: "28px" }} />} title={"Aviable on the "} subtitle={"Apple Store"} />
-                    </div>
                 </div>
                 <div className="col-md-7">
                     <img src={BannerImg} className='img-fluid' alt="banner img " />
